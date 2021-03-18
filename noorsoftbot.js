23d3e028b25f963be297aed1e6b4f9c793798f0a
sevstar2016 <sevstar2016@gmail.com>
2021-03-18 22:08:04 +0500

    0.1

---
+++ noorsoftbot.js
@@ -0,0 +1,11 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf('1615771001:AAF522v6iLHQBChZdvx-kTJ9mmxg6aubor4')
+
+bot.start( ctx => ctx.reply(`
+`))
+
+bot.hears('ping', async (ctx) => {
+        ctx.reply('pong')
+})
+bot.launch()
