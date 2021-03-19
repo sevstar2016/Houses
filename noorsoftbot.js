be407544491004f8ee95777b67fbc03d7189d4a5
sevstar2016 <sevstar2016@gmail.com>
2021-03-19 12:10:15 +0500

    0.2 fix

--- noorsoftbot.js
+++ noorsoftbot.js
@@ -1,7 +1,6 @@
 const { Telegraf } = require('telegraf');
 var Gpio = require('onoff').Gpio
 var LED = new Gpio(4, 'out')
-var blinkInterval = setInterval(blinkLED, 250)
 
 const bot = new Telegraf('1615771001:AAF522v6iLHQBChZdvx-kTJ9mmxg6aubor4')
 
@@ -10,11 +9,7 @@ bot.start( ctx => ctx.reply(`
 
 bot.hears('ping', async (ctx) => {
         blinkLED()
-        endBlink()
 })
-
-bot.launch()
-
 
 function blinkLED() {
         if (LED.readSync() === 0) { 
@@ -26,9 +21,8 @@ function blinkLED() {
 
 
 function endBlink() { 
-        clearInterval(blinkInterval)
         LED.writeSync(0)
         LED.unexport()
 }
 
-setTimeout(endBlink, 5000)
+bot.launch()
