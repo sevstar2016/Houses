e6844cb9e367c645a168144e438f5894267259a3
sevstar2016 <sevstar2016@gmail.com>
2021-03-21 15:46:41 +0500

    ref

--- noorsoftbot.js
+++ noorsoftbot.js
@@ -6,6 +6,8 @@ const bot = new Telegraf('1615771001:AAF522v6iLHQBChZdvx-kTJ9mmxg6aubor4')
 bot.start( ctx => ctx.reply(`
 `))
 
+console.log('started');
+
 bot.hears('ping', async (ctx) => {
         led.blinkLED()
 })
