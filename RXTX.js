4a6aa1021c727ec6aedb3d1b04938355ab257a9c
sevstar2016 <sevstar2016@gmail.com>
2021-03-27 08:41:07 +0500

    Serial RXTX

---
+++ RXTX.js
@@ -0,0 +1,13 @@
+const SerialPort = require('serialport')
+const ReadLine = require('@serialport/parser-readline')
+
+const port = new SerialPort('/dev/ttyACM0', { baundRate:9600 })
+const parser = port.pipe(new ReadLine({delimiter: '\n'}))
+
+port.on("open", () => {
+    console.log('serial port open')
+})
+
+parser.on('data', data => {
+    console.log('got word from arduino: ', data)
+})
