4860c9ac1c5f651283df688fd79fc6fcd4bccf50
sevstar2016 <sevstar2016@gmail.com>
2021-03-27 12:55:51 +0500

    Serial

--- RXTX.js
+++ RXTX.js
@@ -1,7 +1,7 @@
 const SerialPort = require('serialport')
 const ReadLine = require('@serialport/parser-readline')
 
-const port = new SerialPort('/dev/ttyACM0', { baundRate:9600 })
+const port = new SerialPort('/dev/serial0', { baundRate:9600 })
 const parser = port.pipe(new ReadLine({delimiter: '\n'}))
 
 port.on("open", () => {
