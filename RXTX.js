dd4b0f1c25a0035bc4a7b025ba98b68b595b4786
sevstar2016 <sevstar2016@gmail.com>
2021-03-27 10:28:59 +0500

    Serial RXTX

--- RXTX.js
+++ RXTX.js
@@ -4,6 +4,13 @@ const ReadLine = require('@serialport/parser-readline')
 const port = new SerialPort('/dev/serial0', { baundRate:9600 })
 const parser = port.pipe(new ReadLine({delimiter: '\n'}))
 
+port.write('test', (err) => {
+    if(err){
+        return console.log('Error: ', err.message)
+    }
+    console.log('writen')
+})
+
 port.on("open", () => {
     console.log('serial port open')
 })
@@ -12,11 +19,4 @@ parser.on('data', data => {
     if(data == "1"){
         console.log('tested')
     }
-})
-
-port.write('test\n', (err) => {
-    if(err){
-        return console.log('Error: ', err.message)
-    }
-    console.log('writen')
 })
