6c35b5a5fdc72f45dc88544ed6a49310ef4298c2
sevstar2016 <sevstar2016@gmail.com>
2021-03-27 11:50:30 +0500

    Serial

--- RXTX.js
+++ RXTX.js
@@ -1,23 +1,24 @@
-var serial
-const port = '/dev/serial0'
-
+var serial; // instance of the serialport library
+var portName = '/dev/serial0'; // fill in your serial port name here
+ 
+var circleSize = 50;
+ 
 function setup() {
-    serial = new p5.SerialPort();
-    // set callback functions for list and data events:
-    serial.on('list', printList);
-    serial.on('data', serialEvent);
-    // open the serial port:
-    serial.open(port);
+  serial = new p5.SerialPort(document.location.hostname);
+  // set callback functions for list and data events:
+  serial.on('list', printList);
+  serial.on('data', serialEvent);
+  serial.open(portName);
+}
+ 
+function serialEvent() {
+  // read a line of text in from the serial port:
+  var data = serial.readLine();
+  console.log(data);
+}
+ 
+function printList(portList) {
+  for (var i = 0; i < portList.length; i++) {
+    console.log(i + ' ' + portList[i]);
   }
-
-  function serialEvent() {
-    // read a line of text in from the serial port:
-    var data = serial.readLine();
-    console.log(data);
-  }
-   
-  function printList(portList) {
-    for (var i = 0; i < portList.length; i++) {
-      console.log(i + ' ' + portList[i]);
-    }
-  }
+}
