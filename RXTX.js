c4614065f5af7b83f4e6f51049b3b58afa2205d3
sevstar2016 <45399728+sevstar2016@users.noreply.github.com>
2021-04-03 13:06:03 +0300

    fix RXTX

--- RXTX.js
+++ RXTX.js
@@ -1,15 +1,15 @@
 const SerialPort = require('serialport')
 const ReadLine = require('@serialport/parser-readline')
 
-const port = new SerialPort('/dev/serial0', { baundRate:19200 })
+const port = new SerialPort('/dev/ttyUSB0', { baundRate:19200 })
 const parser = port.pipe(new ReadLine({delimiter: '\n'}))
 
 port.on("open", () => {
     console.log('serial port open')
+    port.write('50\n')
+    port.write('2\n')
 })
 
 parser.on('data', data => {
     console.log('got word from arduino:', data)
 })
-
-port.write("1");
