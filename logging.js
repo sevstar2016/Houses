9fb1ccfc7e995ed3ca37ded00d9b4300c38bf56d
sevstar2016 <sevstar2016@gmail.com>
2021-04-25 09:56:36 +0300

    ref login

--- logging.js
+++ logging.js
@@ -23,32 +23,32 @@ class logining {
                         }})
                 
         }
-}
-
-function login(id, pass, message) {
-        var strq = message.split(' ');
-        var dd = false
-                setTimeout(() =>{
-                        if(strq[1] === pass) { 
-                                fs.readFile('test.txt', function (err, data) {
-                                    if (err) throw err;
-                                    if(data.toString().includes(id)){
-                                        console.log(data.toString())
-                                        dd = new Boolean(true)
-                                                if(dd){
-                                                        console.log(dd.toString())
-                                                        return ('1');
-                                                }else{
-                                                        fs.appendFile('test.txt', "\n" + id.toString(), function (err) {
-                                                                if(err) throw err; 
-                                                        })
-                                                        console.log(dd.toString())
-                                                        return ('2');
-                                                }
-                                    }})
-                                }
-                })
-                
+        login(id, pass, message) {
+                var strq = message.split(' ');
+                var dd = new Boolean(false)
+        
+                        setTimeout(() =>{
+                                if(strq[1] === pass) { 
+                                        fs.readFile('test.txt', function (err, data) {
+                                            if (err) throw err;
+                                            if(data.toString().includes(id)){
+                                                console.log(data.toString())
+                                                dd = new Boolean(true)
+                                                        if(dd){
+                                                                console.log(dd.toString())
+                                                                return ('1');
+                                                        }else{
+                                                                fs.appendFile('test.txt', "\n" + id.toString(), function (err) {
+                                                                        if(err) throw err; 
+                                                                })
+                                                                console.log(dd.toString())
+                                                                return ('2');
+                                                        }
+                                            }})
+                                        }
+                        })
+                        
+        }
 }
 
 module.exports = {
