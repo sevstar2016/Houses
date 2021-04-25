a645ba42c3f5f1bef401490609f042bc8b3f5db9
sevstar2016 <sevstar2016@gmail.com>
2021-04-25 20:04:28 +0300

    camera

--- noorsoftbot.js
+++ noorsoftbot.js
@@ -6,29 +6,29 @@ const bot = new Telegraf(PROJ_KEY.toString())
 const logi = new logining(PASS.toString())
 const PiCamera = require('pi-camera');
 const myCamera = new PiCamera({
-        mode: 'photo',
-        output: `${__dirname}/test.jpg`,
-        width: 640,
-        height: 480,
-        nopreview: true,
+    mode: 'photo',
+    output: `${__dirname}/test.jpg`,
+    width: 640,
+    height: 480,
+    nopreview: true,
 });
 
 bot.command('/login', async (ctx) => {
-        logi.login(ctx)
+    logi.login(ctx)
 })
 
 bot.command('/ping', async (ctx) => {
-        ctx.reply(logi.isLogin(ctx))
+    ctx.reply(logi.isLogin(ctx))
 })
 
 bot.command('/shot', async (ctx) => {
-        myCamera.snap()
-                .then((result) => {
-                        ctx.replyWithPhoto(result)
-                })
-                .catch((error) => {
-                        console.log(error)
-                });
+    myCamera.snap()
+        .then((result) => {
+            ctx.replyWithPhoto(result)
+        })
+        .catch((error) => {
+            console.log(error)
+        });
 })
 
 
--- users.js
+++ users.js
@@ -1,22 +1,22 @@
 const fs = require('fs');
 
 class Users {
-    constructor(){
+    constructor() {
         fs.readFile('test.txt', (function (err, data) {
-            if(err) throw err
+            if (err) throw err
             this.users = data.toString().split('\n').slice(1)
             console.log(data.toString())
         }).bind(this))
     }
 
-    addUser(id){
+    addUser(id) {
         this.users.push(id)
         fs.appendFile('test.txt', "\n" + id.toString(), function (err) {
-            if(err) throw err; 
+            if (err) throw err;
         })
     }
 
-    includes(id){
+    includes(id) {
         return this.users.includes(id)
     }
 }
