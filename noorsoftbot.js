3c290bebb2a959b4ceaccc5eecba59d5ae470f8f
sevstar2016 <sevstar2016@gmail.com>
2021-04-25 19:32:02 +0300

    ref all

--- noorsoftbot.js
+++ noorsoftbot.js
@@ -4,7 +4,7 @@ const logining = require('./logging.js').logining
 const {PROJ_KEY, PASS} = process.env;
 
 const bot = new Telegraf(PROJ_KEY.toString())
-const logi = new logining('1')
+const logi = new logining(PASS.toString())
 
 bot.command('/login', async (ctx) => {
         logi.login(ctx)
