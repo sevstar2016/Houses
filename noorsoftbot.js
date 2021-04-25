b5eba1cfe1520fdbffffc151346ab5914077ae97
sevstar2016 <sevstar2016@gmail.com>
2021-04-25 03:03:57 +0300

    camera

--- noorsoftbot.js
+++ noorsoftbot.js
@@ -2,8 +2,18 @@ const { Telegraf } = require('telegraf');
 //const { led } = require('./led.js');
 const fs = require('fs');
 const {logining,login} = require('./logging.js')
+const {PROJ_KEY} = process.env;
 
-const bot = new Telegraf('1615771001:AAF522v6iLHQBChZdvx-kTJ9mmxg6aubor4')
+const PiCamera = require('pi-camera');
+const myCamera = new PiCamera({
+  mode: 'photo',
+  output: `${ __dirname }/test.jpg`,
+  width: 640,
+  height: 480,
+  nopreview: true,
+});
+
+const bot = new Telegraf(PROJ_KEY.toString())
 
 bot.command('/login', async (ctx) => {
         var logi = new logining(ctx.message.text, '1', ctx.message.chat.id.toString())
@@ -15,6 +25,16 @@ bot.command('/ping', async (ctx) => {
         if(logi.isLogin()){
                 reply("pong!")
         }
+})
+
+bot.command('/shot', async (ctx) =>{
+        myCamera.snap()
+  .then((result) => {
+          ctx.replyWithPhoto(result)
+  })
+  .catch((error) => {
+
+  });
 })
 
 
