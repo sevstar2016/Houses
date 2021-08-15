e66eb5267cc620c0ebfec382d95f0c95dd5e9390
sevstar2016 <sevstar2016@gmail.com>
2021-08-15 22:36:02 +0300

    123

--- noorsoftbot.js
+++ noorsoftbot.js
@@ -15,7 +15,7 @@ bot.hears('help', async ({ reply }) => {
     const keyboard = Keyboard.make(['help', 'ping', 'water'], {pattern: [1,2]})
     reply('ping')
     reply('water')
-    await reply('test', keyboard.reply())
+    await reply('', keyboard.reply())
     console.log(keyboard)
 })
 
